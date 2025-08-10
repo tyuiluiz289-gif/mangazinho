5a403ad111146a2117396275485d044ed40b44c7
tyuiluiz289-gif <tyuiluiz289@gmail.com>
2025-08-10 19:09:05 -0300

    Add files via upload
    Versão inicial do Mangazinho PWA

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='mangazinho-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener('fetch',e=>{e.respondWith((async()=>{const cached=await caches.match(e.request);try{const fresh=await fetch(e.request);const c=await caches.open(CACHE);c.put(e.request,fresh.clone());return fresh;}catch{return cached||Response.error();}})())});
